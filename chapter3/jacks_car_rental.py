910e85a680d4fe8255d2f3cf07eed891c954ebf2
habanoz <huseyinabanox@gmail.com>
2022-06-16 18:34:44 +0300

    car rental problem solved.

--- chapter3/jacks_car_rental.py
+++ chapter3/jacks_car_rental.py
@@ -1,6 +1,11 @@
+# copyright huseyinabanox@gmail.com
+# https://github.com/ShangtongZhang/reinforcement-learning-an-introduction/blob/master/chapter04/car_rental.py is used to make sure the solution is correct.
+
 import time
 import numpy as np
 from scipy.stats import poisson
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 LOC2_EXPECTED_RETURN = 2
 LOC2_EXPECTED_REQUEST = 4
@@ -42,16 +47,16 @@ class ProbableUpdate:
 
 def step(action, s, V):
     """
-    :param action: negative values for moving to location 1
-    :param u tuple (req1, req2, ret1, ret2)
-    :param s: current state, not modified
-    :return: tuple of new state and reward
-    """
 
+    :param action: negative actions are towards location 1
+    :param s: state
+    :param V: value matrix
+    :return: action return
+    """
     action_return = -abs(action) * CAR_MOVE_COST
 
-    nLoc1 = s.nLoc1 - action
-    nLoc2 = s.nLoc2 + action
+    nLoc1 = min(s.nLoc1 - action, MAX_CARS)
+    nLoc2 = min(s.nLoc2 + action, MAX_CARS)
 
     for u in states_updates(nLoc1, nLoc2):
         req1, req2, ret1, ret2 = min(u.req1, nLoc1), min(u.req2, nLoc2), u.ret1, u.ret2
@@ -115,6 +120,7 @@ def policy_evaluation(V, pi):
         # if iteration % 10 == 0:
         print("policy_evaluation iteration {}, max delta='{}' in {} seconds"
               .format(iteration, delta, time.time() - istart))
+    print("policy_evaluation completed")
 
 
 def policy_improvement(V, pi):
@@ -125,16 +131,20 @@ def policy_improvement(V, pi):
 
     for s in states():
         old_action = pi[s.nLoc1, s.nLoc2]
-        actions = np.arange(-s.nLoc2, s.nLoc1 + 1)  # negative actions goes to location 1
-        action_returns = np.empty_like(actions)
+        actions = np.arange(-MAX_CARS_MOVED, MAX_CARS_MOVED + 1)  # negative actions goes to location 1
+        action_returns = []
 
-        for a in range(len(actions)):
-            action_return = step(a, s, V)
-            action_returns[a] = action_return
+        for a in actions:
+            if (0 <= a <= s.nLoc1) or (-s.nLoc2 <= a <= 0):
+                action_return = step(a, s, V)
+                action_returns.append(action_return)
+            else:
+                action_returns.append(-1e+10)
 
-        pi[s.nLoc1, s.nLoc2] = actions[np.argmax(action_returns)]
+        new_action = actions[np.argmax(action_returns)]
+        pi[s.nLoc1, s.nLoc2] = new_action
 
-        if pi[s.nLoc1, s.nLoc2] != old_action: policy_stable = False
+        if new_action != old_action: policy_stable = False
 
     print("policy_improvement completed in {} seconds".format(time.time() - start))
 
@@ -145,16 +155,47 @@ def policy_iteration():
     V = np.zeros((MAX_CARS + 1, MAX_CARS + 1))
     pi = np.zeros((MAX_CARS + 1, MAX_CARS + 1), dtype=int)
 
+    _, axes = plt.subplots(2, 3, figsize=(40, 20))
+    plt.subplots_adjust(wspace=0.1, hspace=0.2)
+    axes = axes.flatten()
+    plot_policy(axes, 0, pi)
+
     policy_stable = False
     iteration = 0
     while not policy_stable:
+        iteration += 1
+
         policy_evaluation(V, pi)
         policy_stable = policy_improvement(V, pi)
-        iteration += 1
+
+        if iteration < 5:
+            plot_policy(axes, iteration, pi)
+
         print("Policy iteration {} completed".format(iteration))
 
-    np.savetxt("v.txt", V)
-    np.savetxt("pi.txt", pi)
+    # np.savetxt("v.txt", V)
+    # np.savetxt("pi.txt", pi)
+
+    plot_value_function(V, axes)
+
+    plt.savefig('../images/figure_4_2.png')
+    plt.close()
+
+
+def plot_value_function(V, axes):
+    fig = sns.heatmap(np.flipud(V), cmap="YlGnBu", ax=axes[-1])
+    fig.set_ylabel('# cars at first location', fontsize=30)
+    fig.set_yticks(list(reversed(range(MAX_CARS + 1))))
+    fig.set_xlabel('# cars at second location', fontsize=30)
+    fig.set_title('optimal value', fontsize=30)
+
+
+def plot_policy(axes, iteration, pi):
+    fig = sns.heatmap(np.flipud(pi), cmap="PuBuGn", ax=axes[iteration])
+    fig.set_ylabel('# cars at first location', fontsize=30)
+    fig.set_yticks(list(reversed(range(MAX_CARS + 1))))
+    fig.set_xlabel('# cars at second location', fontsize=30)
+    fig.set_title('policy {}'.format(iteration), fontsize=30)
 
 
 if __name__ == '__main__':
